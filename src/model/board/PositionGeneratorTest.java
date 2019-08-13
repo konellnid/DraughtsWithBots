9def20a0af3a2bd086873238e694d105976f4aaa
Norbert Nawrotek <norbertn15@gmail.com>
2019-08-13 03:25:08 +0200

    Create test skeletons

---
+++ src/model/board/PositionGeneratorTest.java
@@ -0,0 +1,33 @@
+package model.board;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PositionGeneratorTest {
+    PositionGenerator positionGenerator;
+
+    @BeforeEach
+    void init() {
+        positionGenerator = new PositionGenerator();
+    }
+
+    @Test
+    void shouldReturnProperPositionForEightBoardSideLength() {
+
+    }
+
+    @Test
+    void shouldReturnProperPositionForTenBoardSideLength() {
+
+    }
+
+    @Test
+    void shouldReturnProperPositionForTwelveBoardSideLength() {
+
+    }
+
+    @Test
+    void shouldReturnNullForUnavailableBoardSideLength() {
+
+    }
+}
