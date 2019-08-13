5857f428e4658ca09be2089eb9189b23a1acb834
Norbert Nawrotek <norbertn15@gmail.com>
2019-08-13 19:46:30 +0200

    Add test skeleton for board size 12

--- src/model/move_finder/PossibleMovesFinderTest.java
+++ src/model/move_finder/PossibleMovesFinderTest.java
@@ -397,5 +397,36 @@ class PossibleMovesFinderTest {
         assertEquals(expectedMoveList, actualMoveList);
     }
 
+    // ________________________________________________________________________
+    // TESTS FOR BOARD SIDE LENGTH = 12
+    /*
+    Board of sideLength = 12: total BitSet size = 91
+      |  83  82  81  80  79  78|
+      |77  76  75  74  73  72  |
+      |  70  69  68  67  66  65|
+      |64  63  62  61  60  59  |
+      |  57  56  55  54  53  52|
+      |51  50  49  48  47  46  |
+      |  44  43  42  41  40  39|
+      |38  37  36  35  34  33  |
+      |  31  30  29  28  27  26|
+      |25  24  23  22  21  20  |
+      |  18  17  16  15  14  13|
+      |12  11  10  09  08  07  |
+     */
+
+    @Test
+    void should() {
+        prepareBitSetsAndPositionFinderForBoardSideLength(BOARD_SIDE_LENGTH_TWELVE);
+
+
+
+        actualMoveList = possibleMovesFinder.getAvailableMovesFrom(position, );
+
+        sortBothLists();
+
+        assertEquals(expectedMoveList, actualMoveList);
+    }
+
 
 }
