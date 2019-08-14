061043ce77d55738234abfddf359323102b8fda7
Norbert Nawrotek <norbertn15@gmail.com>
2019-08-15 01:09:45 +0200

    Add getting basis BitSets from Position

--- src/model/bot/position_rater/PositionRater.java
+++ src/model/bot/position_rater/PositionRater.java
@@ -2,12 +2,18 @@ package model.bot.position_rater;
 
 import model.board.Position;
 
+import java.util.BitSet;
+
 public class PositionRater {
     public static final int WHITE_WON = Integer.MAX_VALUE;
     public static final int BLACK_WON = Integer.MIN_VALUE;
 
     private PositionRaterSettings positionRaterSettings;
     private int boardSideLength;
+    private BitSet whiteCheckers;
+    private BitSet whiteKings;
+    private BitSet blackCheckers;
+    private BitSet blackKings;
 
     public PositionRater(PositionRaterSettings positionRaterSettings, int boardSideLength) {
         this.positionRaterSettings = positionRaterSettings;
@@ -15,6 +21,25 @@ public class PositionRater {
     }
 
     public int getRatingOfPosition(Position position) {
+        prepareWhiteBitSets(position.getWhitePieces(), position.getKings());
+        prepareBlackBitSets(position.getBlackPieces(), position.getKings());
+
         return 0;
+    }
+
+    private void prepareWhiteBitSets(BitSet whitePieces, BitSet kings) {
+        whiteCheckers = (BitSet) whitePieces.clone();
+        whiteCheckers.andNot(kings);
+
+        whiteKings = (BitSet) whitePieces.clone();
+        whiteKings.and(kings);
+    }
+
+    private void prepareBlackBitSets(BitSet blackPieces, BitSet kings) {
+        whiteCheckers = (BitSet) blackPieces.clone();
+        whiteCheckers.andNot(kings);
+
+        whiteKings = (BitSet) blackPieces.clone();
+        whiteKings.and(kings);
     }
 }
