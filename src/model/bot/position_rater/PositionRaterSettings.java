f4d89f101c37295f3a874726078fcde263209f36
Norbert Nawrotek <norbertn15@gmail.com>
2019-08-14 22:44:09 +0200

    Add PositionRater test class

--- src/model/bot/position_rater/PositionRaterSettings.java
+++ src/model/bot/position_rater/PositionRaterSettings.java
@@ -3,7 +3,7 @@ package model.bot.position_rater;
 public class PositionRaterSettings {
     private int pointsPerChecker;
     private int pointsPerKing;
-    private boolean isBonusForBeingCloserToPromotionLineActive; // from 0 to 6 points, the closer the checker is to the promotion line, the more points
+    private boolean isBonusForBeingCloserToPromotionLineActive; // 0 at the lowest row for given colour, +1 for each row closer to the promotion row
     private boolean isBonusForKingControllingMainDiagonalActive; // 5 points if player has king on the main diagonal and enemy has no pieces on it
 
     public PositionRaterSettings() {
---
+++ src/model/bot/position_rater/PositionRaterTest.java
@@ -0,0 +1,63 @@
+package model.bot.position_rater;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PositionRaterTest {
+    private PositionRaterSettings positionRaterSettings;
+    private PositionRater positionRater;
+
+    @BeforeEach
+    void setUp() {
+        positionRaterSettings = new PositionRaterSettings();
+    }
+
+
+    // BOARD SIDE LENGTH = 8 TESTS
+    /*
+      |  39  38  37  36|
+      |35  34  33  32  |
+      |  30  29  28  27|
+      |26  25  24  23  |
+      |  21  20  19  18|
+      |17  16  15  14  |
+      |  12  11  10  09|
+      |08  07  06  05  |
+     */
+    @Test
+    void shouldReturnProperRatingForDefaultSettings() {
+    }
+
+    // BOARD SIDE LENGTH = 10 TESTS
+    /*
+      |  59  58  57  56  55|
+      |54  53  52  51  50  |
+      |  48  47  46  45  44|
+      |43  42  41  40  39  |
+      |  37  36  35  34  33|
+      |32  31  30  29  28  |
+      |  26  25  24  23  22|
+      |21  20  19  18  17  |
+      |  15  14  13  12  11|
+      |10  09  08  07  06  |
+     */
+
+    // BOARD SIDE LENGTH = 12 TESTS
+    /*
+      |  83  82  81  80  79  78|
+      |77  76  75  74  73  72  |
+      |  70  69  68  67  66  65|
+      |64  63  62  61  60  59  |
+      |  57  56  55  54  53  52|
+      |51  50  49  48  47  46  |
+      |  44  43  42  41  40  39|
+      |38  37  36  35  34  33  |
+      |  31  30  29  28  27  26|
+      |25  24  23  22  21  20  |
+      |  18  17  16  15  14  13|
+      |12  11  10  09  08  07  |
+     */
+
+}
