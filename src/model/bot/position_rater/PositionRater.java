ef025080c063d5ceffdae18be024d4a58eb22c12
Norbert Nawrotek <norbertn15@gmail.com>
2019-08-14 21:29:59 +0200

    Create PositionRater class skeleton

--- src/model/bot/position_rater/PositionRater.java
+++ src/model/bot/position_rater/PositionRater.java
@@ -1,6 +1,19 @@
 package model.bot.position_rater;
 
-public class PositionRater {
-    private PositionRaterSettings positionRaterSettings;
+import model.board.Position;
 
+public class PositionRater {
+    private static final int WHITE_WON = Integer.MAX_VALUE;
+    private static final int BLACK_WON = Integer.MIN_VALUE;
+
+    private PositionRaterSettings positionRaterSettings;
+    private int boardSideLength;
+
+    public PositionRater(int boardSideLength) {
+        this.boardSideLength = boardSideLength;
+    }
+
+    public int getRatingOfPosition(Position position) {
+        return 0;
+    }
 }
